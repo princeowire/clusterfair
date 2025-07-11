e3d803a1f2b9b6d502fcbca4235488bf2a33f2ad
Prince Owire <princeowire131@gmail.com>
2025-07-11 12:42:45 +0100

    Add global styles and layout structure for Next.js application
    - Created globals.css for base styles and imported Tailwind CSS.
    - Implemented RootLayout component in layout.js to define HTML structure and font imports.
    - Added Home component in page.js with a placeholder for the main content and included Navbar component.

---
+++ src/app/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Menu, X } from "lucide-react";
+import { motion } from "framer-motion";
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const navLinks = ["Home", "About", "Services", "Blog", "Contact"];
+
+  return (
+    <nav className="">
+      appWIse
+
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+"use client";
+
+import Image from "next/image";
+import Navbar from "./components/navbar";
+
+export default function Home() {
+  return (
+    <div className="">
+      <Navbar />
+      <main className="corner-cut-box">
+        
+      </main>
+    </div>
+  );
+}
